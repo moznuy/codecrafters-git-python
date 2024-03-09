d385a940682ee76815a30e54a5d835e242ba3c5b
Serhii Charykov <serhii.charykov@gmail.com>
2024-03-09 12:35:56 +0200

    3rd stage

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import hashlib
 import sys
 import os
 import zlib
@@ -21,16 +22,43 @@ def cat_file():
     with open(f".git/objects/{folder}/{file}", "rb") as f:
         compressed = f.read()
     raw_content = zlib.decompress(compressed)
+
     header: bytes
     content: bytes
     header, content = raw_content.split(b'\0', maxsplit=1)
+
     file_type: str
     size_raw: str
     file_type, size_raw = header.decode().split(maxsplit=1)
     size = int(size_raw)
+    assert file_type == 'blob'
+    assert size == len(content)
+
     # Raw bytes output:
     sys.stdout.buffer.write(content)
     sys.stdout.flush()
+
+
+def hash_object():
+    w = sys.argv[2]
+    filename = sys.argv[3]
+    assert w == "-w"
+
+    with open(filename, "rb") as f:
+        content = f.read()
+
+    size = len(content)
+    header = f"blob {size}".encode()
+    raw_content = header + b'\0' + content
+    digest = hashlib.sha1(raw_content).hexdigest()
+    compressed = zlib.compress(raw_content)
+
+    folder, file = digest[:2], digest[2:]
+
+    os.makedirs(f".git/objects/{folder}", exist_ok=True)
+    with open(f".git/objects/{folder}/{file}", 'wb') as f:
+        f.write(compressed)
+    print(digest)
 
 
 def main():
@@ -39,6 +67,8 @@ def main():
         init()
     elif command == "cat-file":
         cat_file()
+    elif command == "hash-object":
+        hash_object()
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
